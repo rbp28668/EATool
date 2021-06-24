ea9a2bd08fe76d56ca9226938b5a7dd65c325752
rbp28668 <bruce.porteous@ntlworld.com>
2021-06-24 08:45:34 +0100

    scripts now uses DTOs

--- src/alvahouse/eatool/repository/persist/ScriptPersistence.java
+++ src/alvahouse/eatool/repository/persist/ScriptPersistence.java
@@ -5,7 +5,7 @@ package alvahouse.eatool.repository.persist;
 
 import java.util.Collection;
 
-import alvahouse.eatool.repository.scripting.Script;
+import alvahouse.eatool.repository.dto.scripting.ScriptDto;
 import alvahouse.eatool.util.UUID;
 
 /**
@@ -25,7 +25,7 @@ public interface ScriptPersistence {
 	 * @param key is the key of the script to get
 	 * @return the corresponding script, or null if not in the model
 	 */
-	public Script getScript(UUID key) throws Exception;
+	public ScriptDto getScript(UUID key) throws Exception;
 
 	/**
 	 * adds a new script to the collection.
@@ -33,7 +33,7 @@ public interface ScriptPersistence {
 	 * @param s is the script to add.
 	 * @throws IllegalStateException if the script already exists in the model.
 	 */
-	public void addScript(Script s) throws Exception;
+	public void addScript(ScriptDto s) throws Exception;
 
 	/**
 	 * Updates and existing script in the model.
@@ -41,7 +41,7 @@ public interface ScriptPersistence {
 	 * @param s is the script to update.
 	 * @throws IllegalStateException if the script already exists in the model.
 	 */
-	public void updateScript(Script s) throws Exception;
+	public void updateScript(ScriptDto s) throws Exception;
 
 	/**
 	 * Deletes an script keyed by UUID.
@@ -56,7 +56,7 @@ public interface ScriptPersistence {
 	 * 
 	 * @return an iterator for the entities.
 	 */
-	public Collection<Script> getScripts() throws Exception;
+	public Collection<ScriptDto> getScripts() throws Exception;
 
 	/**
 	 * DEBUG only
